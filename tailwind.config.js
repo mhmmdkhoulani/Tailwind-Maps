4dbb145da73ab92f935c102136b4f97569bbc77e
Mohammad <mhmmdkholani@gmail.com>
2024-06-01 16:52:44 +0300

    Tailwaind Starter

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        MainColor: "#7f71d1",
+      },
+    },
+  },
+
+  plugins: [],
+};
